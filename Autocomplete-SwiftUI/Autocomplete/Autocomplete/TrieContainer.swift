243c4a48055fd7f0b4d8b5b855578bda36fbbb25
Ethan Hess <ech1988@gmail.com>
2023-04-27 14:06:24 -0700

    Add node row for recursion but need to adjust coordinates

--- Autocomplete-SwiftUI/Autocomplete/Autocomplete/TrieContainer.swift
+++ Autocomplete-SwiftUI/Autocomplete/Autocomplete/TrieContainer.swift
@@ -16,29 +16,35 @@ struct TrieContainer: View {
             GeometryReader { geometry in
                 VStack {
                     ZStack {
+                        let geoSize = geometry.size
                         if viewModel.trie != nil {
                             let root = viewModel.trie!.root
-                            let rootPointX = (geometry.size.width / 2) - 20
+                            let rootPointX = (geoSize.width / 2) - 20
                             let startPointRoot = CGPoint(x: rootPointX, y: 100)
-                            let endPointRoot = CGPoint(x: rootPointX, y: offsetYForIndex(1))
+                            let endPointRoot = CGPoint(x: rootPointX, y: NodeHelper.offsetYForIndex(1))
                             
-                            NodeUI(char: root.value).frame(width: 40, height: 40, alignment: .center).cornerRadius(20).offset(x: rootPointX, y: offsetYForIndex(1)).overlay(alignment: .center) {
+                            NodeUI(char: root.value).frame(width: 40, height: 40, alignment: .center).cornerRadius(20).offset(x: rootPointX, y: NodeHelper.offsetYForIndex(1)).overlay(alignment: .center) {
                                 Path { path in
                                     path.move(to: startPointRoot)
                                     path.addLine(to: endPointRoot)
                                     path.closeSubpath()
                                 }.stroke(.white, lineWidth: 2)
                             }
+            
                             
-                            let allKeys = Array(root.children.keys)
+                            //MARK: Other approach (Keep for reference then discard)
+                            
+                            //let allKeys = Array(root.children.keys)
                             //Need to recur and get children of children etc. for each letter to build tree
-                            
-                            let arr = getIdentifiableArrayFromKeys(allKeys)
-                            
+
+                            let arr = NodeHelper.getIdentifiableArrayFromKeys(root)
+
                             ForEach(0..<arr.count, id: \.self) { i in
-                                let offsetX = offsetXForIndex(i)
-                                let offsetY = offsetYForIndex(2)
-                                NodeUI(char: arr[i].key).frame(width: 40, height: 40, alignment: .center).cornerRadius(20).offset(x: offsetX, y: offsetY).overlay(alignment: .center) {
+                                //Top row (root's children, can use recursive builder function for this too but just seeing how it looks / works this way)
+                                let nodeAtIndex = arr[i]
+                                let offsetX = NodeHelper.offsetXForIndex(i)
+                                let offsetY = NodeHelper.offsetYForIndex(2)
+                                NodeUI(char: nodeAtIndex.key).frame(width: 40, height: 40, alignment: .center).cornerRadius(20).offset(x: offsetX, y: offsetY).overlay(alignment: .center) {
                                     //won't add path to last node
                                     if i < (arr.count - 1) {
                                         let startPointChild = CGPoint(x: rootPointX, y: 100)
@@ -52,6 +58,14 @@ struct TrieContainer: View {
                                         Spacer()
                                     }
                                 }
+                                
+                                //MARK: Recur until end of trie (shows nodes but need to adjust coordinates)
+                                
+//                                if !nodeAtIndex.val.children.isEmpty {
+//                                    NodeRow(node: nodeAtIndex.val, x: offsetX, y: offsetY, depth: 3).frame(width: geoSize.width, height: 60).offset(x: 0)
+//                                } else {
+//                                    EmptyView()
+//                                }
                             }
                         }
                     }
@@ -76,37 +90,8 @@ struct TrieContainer: View {
         print("New Trie \(newVal)")
     }
     
-    //Will want to set x / y separately since one may be altered by index but not the other
-    fileprivate func offsetYForIndex(_ layer: Int) -> CGFloat {
-        return CGFloat(layer) * 60
-    }
-    
-    fileprivate func offsetXForIndex(_ i: Int) -> CGFloat {
-        return CGFloat(i) * 60
-    }
-    
     //MARK: TODO imp. get w / h of tree and size nodes accordingly (i.e. if bigger tree make nodes smaller and fit into view)
-//    fileprivate func getFullWidthOfTrie(_ trie: Trie) -> Int {
-//
-//    }
-//
-//    fileprivate func getFullHeightOfTrie(_ trie: Trie) -> Int {
-//
-//    }
-    
-    //If tree grows this is not efficient, make sure original map is identifiable to not iterate twice
 
-    //For a small tree example / workaround this is okay though
-    
-    fileprivate func getIdentifiableArrayFromKeys(_ allKeys: [String]) -> [IdentifiableDictionary] {
-        var returnArray : [IdentifiableDictionary] = []
-        for str in allKeys {
-            let dict = IdentifiableDictionary(key: str)
-            returnArray.append(dict)
-        }
-        return returnArray
-    }
-    
     //"some" just means it conforms to View protocol, it could be VStack / HStack / Text etc. with whatever children but we know View is the opaque type, no need to be any more specific and expose type info
     
 //    fileprivate func bezierPathBetweenViews(_ viewOne: some View, viewTwo: some View, geometry: GeometryProxy) -> Path {
@@ -115,7 +100,7 @@ struct TrieContainer: View {
 //    }
 }
 
-//TODO move to own file
+//MARK: Helper UI, TODO move to own file
 struct NodeUI: View {
     @State var char : String
     // @State var shouldHighlight: Bool = false // when traversing tree
@@ -128,7 +113,77 @@ struct NodeUI: View {
     }
 }
 
+//MARK: Recur children all the way down view until end of trie, organize into rows (depth)
+struct NodeRow: View {
+    
+    @State var node : Node
+    @State var x : CGFloat
+    @State var y : CGFloat
+    @State var depth : CGFloat
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let geoSize = geometry.size
+            HStack {
+                let arr = NodeHelper.getIdentifiableArrayFromKeys(node)
+                ForEach(0..<arr.count, id: \.self) { i in
+                    let nodeAtIndex = arr[i]
+                    let offsetX = NodeHelper.offsetXForIndex(i)
+                    let offsetY = NodeHelper.offsetYForIndex(Int(depth))
+                    NodeUI(char: nodeAtIndex.key).frame(width: 40, height: 40, alignment: .center).cornerRadius(20).offset(x: offsetX, y: offsetY).overlay(alignment: .center) {
+                        //won't add path to last node
+                        if i < (arr.count - 1) {
+                            let startPointChild = CGPoint(x: x, y: y)
+                            let endPointChild = CGPoint(x: offsetX, y: offsetY)
+                            Path { path in
+                                path.move(to: startPointChild)
+                                path.addLine(to: endPointChild)
+                                path.closeSubpath()
+                            }.stroke(.white, lineWidth: 2)
+                        } else {
+                            Spacer()
+                        }
+                    }
+                    
+                    if !nodeAtIndex.val.children.isEmpty {
+                        NodeRow(node: nodeAtIndex.val, x: offsetX, y: offsetY + 60, depth: depth + 1).frame(width: geoSize.width, height: 60).offset(x: 0)
+                    } else {
+                        EmptyView()
+                    }
+                }
+            }
+        }
+    }
+}
+
+//MARK: Helper struct (not UI related)
 struct IdentifiableDictionary : Identifiable {
     var id = ""
     var key : String
+    var val : Node
+}
+
+struct NodeHelper {
+    //Will want to set x / y separately since one may be altered by index but not the other
+    static func offsetYForIndex(_ layer: Int) -> CGFloat {
+        return CGFloat(layer) * 60
+    }
+    
+    static func offsetXForIndex(_ i: Int) -> CGFloat {
+        return CGFloat(i) * 60
+    }
+
+    //If tree grows this is not efficient, make sure original map is identifiable to not iterate twice
+
+    //For a small tree example / workaround this is okay though
+    static func getIdentifiableArrayFromKeys(_ node: Node) -> [IdentifiableDictionary] {
+        var returnArray : [IdentifiableDictionary] = []
+        let allKeys = node.children.keys
+        for str in allKeys {
+            let val = node.children[str]!
+            let dict = IdentifiableDictionary(key: str, val: val)
+            returnArray.append(dict)
+        }
+        return returnArray
+    }
 }
